740ab2e43f0fb4c360eb479bc7372c49a6674007
ralexeich <57177827+ralexeich@users.noreply.github.com>
2020-04-19 23:44:17 +0300

    Create populate.sql

---
+++ populate.sql
@@ -0,0 +1,74 @@
+insert into track(track_name,track_duration,artist)
+values ('DHL', '183', 'Frank Ocean');
+
+insert into track(track_name,track_duration,artist)
+values ('GUMMY', '156', 'BROCKHAMPTON');
+
+insert into track(track_name,track_duration,artist)
+values ('GEORGIA', '147', 'Kevin Abstract');
+
+insert into track(track_name,track_duration,artist)
+values ('Cloud Blu', '137', 'IDK');
+
+insert into track(track_name,track_duration,artist)
+values ('Stoned Again', '168', 'King Krule');
+
+insert into track(track_name,track_duration,artist)
+values ('Ivy', '198', 'Frank Ocean');
+
+
+
+
+insert into region(region_name, full_spreading)
+values ('Apple Music', 'Yes');
+
+insert into region(region_name, full_spreading)
+values ('Spotify', 'No');
+
+insert into region(region_name, full_spreading)
+values ('Tidal', 'Yes');
+
+insert into region(region_name, full_spreading)
+values ('Google Music', 'Yes');
+
+insert into region(region_name, full_spreading)
+values ('Amazon Music', 'No');
+
+insert into region(region_name, full_spreading)
+values ('YouTube Music', 'Yes');
+
+insert into region(region_name, full_spreading)
+values ('Soundcloud', 'Yes');
+
+
+
+
+insert into charts(charts_place, region_name, track_name, popularity)
+values ('1', 'Apple Music', 'DHL', '79');
+
+insert into charts(charts_place, region_name, track_name, popularity)
+values ('4', 'Apple Music', 'Cloud Blu', '58');
+
+insert into charts(charts_place, region_name, track_name, popularity)
+values ('6', 'Spotify', 'Ivy', '72');
+
+insert into charts(charts_place, region_name, track_name, popularity)
+values ('8', 'Tidal', 'GUMMY', '55');
+
+insert into charts(charts_place, region_name, track_name, popularity)
+values ('3', 'Spotify', 'GEORGIA', '67');
+
+insert into charts(charts_place, region_name, track_name, popularity)
+values ('2', 'Apple Music', 'Stoned Again', '45');
+
+
+
+
+
+
+
+
+
+
+
+
