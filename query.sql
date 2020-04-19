49361b697d0d83cabf11eb1eb96a5d7007ab2def
ralexeich <57177827+ralexeich@users.noreply.github.com>
2020-04-20 01:19:18 +0300

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,21 @@
+-- query1 
+select sum(track_duration) as all_playlist
+from track
+where artist= 'Frank Ocean';
+
+
+
+-- query2
+select region_name, round((count(region_name))/(select count(*) from region)*100, 2) persent
+from region
+group by region_name
+order by persent DESC , region_name
+
+
+
+
+-- query3
+select charts_place, max(popularity) as  popular
+from charts
+group by charts_place
+order by charts_place;
