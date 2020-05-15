f3d8f0d85703ce482b8b8ae8092440d9cda7f7e0
ralexeich <57177827+ralexeich@users.noreply.github.com>
2020-05-15 13:39:23 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,16 +1,18 @@
 create table track(  
-      track_name varchar(50) not null PRIMARY KEY, 
-      artist varchar(50) not null, 
+      track_name varchar(1000) not null PRIMARY KEY, 
+      artist varchar(1000) not null, 
       track_duration integer not null 
 );
 create table region(
-      region_name varchar(50) not null PRIMARY KEY,
+      region_name varchar(1000) not null PRIMARY KEY,
       full_spreading varchar(50) not null
 );
      
 create table charts(
       charts_place integer not null PRIMARY KEY,
-      region_name varchar(50) not null references region(region),
-      track_name varchar(50) not null references track(track_name),
+      region_name varchar(1000) not null references region(region_name),
+      track_name varchar(1000) not null references track(track_name),
       popularity integer not null
 );
+
+
